7b2d81c7a797f93e774de2a798cb7d1d52e2569d
mkan0141 <kanzume0141@gmail.com>
2022-11-03 14:24:24 +0900

    next-blog chore(pages): フォントの調整

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body className="font-note-sans-jp">
+      <Head></Head>
+      <body>
         <Main></Main>
         <NextScript></NextScript>
       </body>
