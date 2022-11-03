3c32a1b0fbea473b0dbc8b44fc1d9ce5d6ed69e3
mkan0141 <kanzume0141@gmail.com>
2022-11-03 14:15:46 +0900

    next-blog feat(): add layout component

---
+++ components/layout.tsx
@@ -0,0 +1,19 @@
+import Header from "components/header";
+import Footer from "components/footer";
+import Container from "components/container";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <Container>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Layout;
