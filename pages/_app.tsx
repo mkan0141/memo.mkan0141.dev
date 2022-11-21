08326ec8edc485abe92b5e6366a1e93a0c21b0b5
mkan0141 <kanzume0141@gmail.com>
2022-11-22 00:01:14 +0900

    #12 chore(): add google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'components/layout';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-P2YRZ64G57"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-P2YRZ64G57');
+        `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
